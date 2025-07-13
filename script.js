f5ae528d7eab16761ce8a7600a4635c3a6e70517
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-07-13 21:30:48 +0530

    added animations

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet");
-Shery.hoverWithMediaCircle(".hvr", {videos: ["./assets/0.mp4","./assets/2.mp4","./assets/3.mp4" ]});
+Shery.hoverWithMediaCircle(".hvr", { videos: ["./assets/0.mp4", "./assets/2.mp4", "./assets/3.mp4"] });
+
+gsap.to(".fleft-elem", {
+    scrollTrigger: {
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1
+    },
+    y: "-300%",
+    ease: Power1
+})
+
+
+let sections = document.querySelectorAll(".fleft-elem");
+Shery.imageEffect(".images", {
+    style: 4,
+    debug: false,
+    slideStyle: (setScroll) => {
+        sections.forEach(function(section, index){
+           ScrollTrigger.create({
+            trigger: section,
+            start: "top top",
+            scrub: 1,
+            onUpdate: function(prog){
+                setScroll(prog.progress+index)
+            }
+           }) 
+        });
+    },
+});
+
